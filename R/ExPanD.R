de5237d9a5526523dffe990a6851333c605fd16f
Rishi Das Roy <rishi.dasroy@gmail.com>
2021-08-25 14:58:06 +0300

    Run in server

--- R/ExPanD.R
+++ R/ExPanD.R
@@ -398,5 +398,6 @@ ExPanD <- function(df = NULL, cs_id = NULL, ts_id = NULL,
   app_dir <- paste0(tempdir(), "/application")
   save(list = ls(pattern = "shiny"), file = paste0(app_dir, "/shiny_data.Rda"))
   on.exit(unlink(app_dir, recursive = TRUE))
-  try(shiny::runApp(appDir = app_dir, ...))
+  #try(shiny::runApp(appDir = app_dir, ...))
+  return(app_Dir)
 }
